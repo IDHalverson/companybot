e22bab26d28808951134ff6acb2c272db31243e3
Ian Halverson <ihalverson@burrislogistics.com>
2020-04-24 13:25:33 -0400

    test burris bot responses

--- index.js
+++ index.js
@@ -6,19 +6,34 @@ const app = new App({
   signingSecret: process.env.SLACK_SIGNING_SECRET
 });
 
+const getRandomInt = (max) => {
+  return Math.floor(Math.random() * Math.floor(max));
+}
 
+const burrisBotGreetings = [
+  "Make It Happen! :burris_snowflake_png:",
+  "Get It Right! :burris_snowflake_png:",
+  "I Am Burris! :burris_snowflake_png:"
+]
 
 // code here
 
-app.event('message.channels', async ({event, context}) => {
-  try {
-    console.log(event);
-    console.log(context);
-  } catch (e) {
-    console.error(e)
-  }
-});
+// app.event('message.channels', async ({event, context}) => {
+//   try {
+//     console.log(event);
+//     console.log(context);
+//   } catch (e) {
+//     console.error(e)
+//   }
+// });
 
+app.message(/(.)*burris(\-)*bot(.)*/, async ({ context, say }) => {
+  let greeting = burrisBotGreetings[getRandomInt(burrisBotGreetings.length)];
+  if (!greeting) {
+    greeting = burrisBotGreetings[getRandomInt(burrisBotGreetings.length)];
+  }
+  await say(`${greeting}`);
+});
 
 (async () => {
   await app.start(process.env.PORT || 3000);
