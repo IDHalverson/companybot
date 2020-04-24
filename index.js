b0dbe8413b3779291958d1aef5054af1bbd7c24e
Ian Halverson <ihalverson@burrislogistics.com>
2020-04-24 14:52:37 -0400

    add nodemailer and test it

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 require("dotenv").config();
+const nodemailer = require("nodemailer");
 const { App } = require("@slack/bolt");
 const BurrisBot = require("./burris-bot");
 
 const burrisBot = new BurrisBot();
+
+const transporter = nodemailer.createTransport({
+  host: "gmail",
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS
+  }
+});
+const defaultMailOptions = {
+  from: "burrisbot@gmail.com",
+  to: "ihalverson@burrislogistics.com",
+  subject: "Support Request from Slack: "
+};
 
 const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
@@ -20,9 +34,26 @@ const app = new App({
 //   }
 // });
 
-app.message(/(.)*burris(\-)*bot(.)*/, async ({ context, say }) => {
-  await say(`${burrisBot.getBurrisBotGreeting()}`);
+app.message(/(.)*burris(\-|\_)*bot(.)*/i, async ({ context, say }) => {
+  await say(burrisBot.getBurrisBotGreeting());
 });
+
+app.message(
+  /(.)*\@\@email(\-|\_)*solutions\ (.)*/i,
+  async ({ context, say }) => {
+    const supportRequest = context.matches[2];
+
+    const mailOptions = {
+      ...defaultMailOptions,
+      subject: defaultMailOptions.subject + `${supportRequest}`,
+      html: `<p>${supportRequest}</p>`
+    };
+
+    transporter.sendMail(mailOptions, (err, info) => {
+      if (err) console.error(err);
+    });
+  }
+);
 
 (async () => {
   await app.start(process.env.PORT || 3000);
