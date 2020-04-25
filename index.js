e29bcd7abb01a6e2db6c2657812ed758a5fb8480
Ian Halverson <ihalverson@burrislogistics.com>
2020-04-25 01:15:02 -0400

    make using @solutions cause a Tip from burris bot

--- index.js
+++ index.js
@@ -32,32 +32,25 @@ app.message(/(.)*burris(\-|\_)*bot(.)*/i, async ({ context, say }) => {
 });
 
 app.message(
-  /(.)*\@\@email(\-|\_)*solutions\ (.)*/i,
-  async ({ context, say }) => {
-    const supportRequest = context.matches[3];
-    const mailOptions = {
-      ...defaultMailOptions,
-      subject:
-        defaultMailOptions.subject +
-        `${supportRequest} (${moment(Date.now()).format(
-          "MM/DD/YYYY @ hh:mma"
-        )})`,
-      html: `<p>${supportRequest}</p>`
-    };
-    transporter.sendMail(mailOptions, (err, info) => {
-      if (err) console.error(err);
+  /.*\@(?:email)?(?:\-|\_|\ )?solutions\ (.*)/i,
+  async ({ context, body, ack, say, payload }) => {
+    const channelInfo = await app.client.channels.info({
+      token: context.botToken,
+      channel: body.event.channel
+    });
+    const channel_id = channelInfo.channel.id;
+    const user_id = payload.user;
+    const text = context.matches[1];
+    await app.client.chat.postEphemeral({
+      token: context.botToken,
+      channel: channel_id,
+      user: user_id,
+      text: `:point_up: Tip: Try typing \`/solutions ${text}\` to send an email to ${process.env.SOLUTIONS_EMAIL}.`
     });
   }
 );
 
-const getUserContext = async ({
-  payload,
-  context,
-  command,
-  next,
-  ack,
-  say
-}) => {
+const getUserContext = async ({ context, command, next, ack, say }) => {
   try {
     const user = await app.client.users.info({
       token: context.botToken,
@@ -77,142 +70,146 @@ const getUserContext = async ({
   }
 };
 
-app.command(
-  "/solutions",
-  getUserContext,
-  async ({ context, command, body, ack, say }) => {
-    await ack();
-    try {
-      const textInline = command.text
-        .replace(/\n/g, " ")
-        .replace(/[\s]{2,}/g, " ");
-      const initialSubject =
-        defaultMailOptions.subject +
-        `${(textInline || "").substring(0, 60)}${
-          (textInline || "").length > 59 ? "..." : ""
-        }`;
-      const initialBody = command.text;
+const mailSolutionsWorkflow = async ({ context, command, body, ack, say }) => {
+  try {
+    const textInline = command.text
+      .replace(/\n/g, " ")
+      .replace(/[\s]{2,}/g, " ");
+    const initialSubject =
+      defaultMailOptions.subject +
+      `${(textInline || "").substring(0, 60)}${
+        (textInline || "").length > 59 ? "..." : ""
+      }`;
+    const initialBody = command.text;
 
-      const result = await app.client.views.open({
-        token: context.botToken,
-        trigger_id: body.trigger_id,
-        view: {
-          callback_id: "solutions_email",
-          type: "modal",
-          title: {
-            type: "plain_text",
-            text: "Email Burris Solutions",
-            emoji: true
+    const result = await app.client.views.open({
+      token: context.botToken,
+      trigger_id: body.trigger_id,
+      view: {
+        callback_id: "solutions_email",
+        type: "modal",
+        title: {
+          type: "plain_text",
+          text: "Email Burris Solutions",
+          emoji: true
+        },
+        private_metadata: `${context.user_real_name}<sep?>${command.channel_name}<sep?>${context.user_email}<sep?>${context.user_id}<sep?>${command.channel_id}`,
+        submit: {
+          type: "plain_text",
+          text: "Submit",
+          emoji: true
+        },
+        close: {
+          type: "plain_text",
+          text: "Cancel",
+          emoji: true
+        },
+        blocks: [
+          {
+            type: "section",
+            text: {
+              type: "mrkdwn",
+              text: `:email: *Email to ${process.env.SOLUTIONS_EMAIL}:*`
+            }
           },
-          private_metadata: `${context.user_real_name}<sep?>${command.channel_name}<sep?>${context.user_email}<sep?>${context.user_id}<sep?>${command.channel_id}`,
-          submit: {
-            type: "plain_text",
-            text: "Submit",
-            emoji: true
-          },
-          close: {
-            type: "plain_text",
-            text: "Cancel",
-            emoji: true
-          },
-          blocks: [
-            {
-              type: "section",
-              text: {
-                type: "mrkdwn",
-                text: `:email: *Email to ${process.env.SOLUTIONS_EMAIL}:*`
-              }
+          {
+            type: "input",
+            block_id: "subject",
+            element: {
+              type: "plain_text_input",
+              action_id: "subject_value",
+              initial_value: initialSubject
             },
-            {
-              type: "input",
-              block_id: "subject",
-              element: {
-                type: "plain_text_input",
-                action_id: "subject_value",
-                initial_value: initialSubject
+            label: {
+              type: "plain_text",
+              text: "Subject:",
+              emoji: false
+            }
+          },
+          {
+            type: "input",
+            block_id: "body",
+            element: {
+              type: "plain_text_input",
+              action_id: "body_value",
+              multiline: true,
+              initial_value: initialBody
+            },
+            label: {
+              type: "plain_text",
+              text: "Body:",
+              emoji: false
+            }
+          },
+          {
+            type: "input",
+            block_id: "urgency",
+            element: {
+              type: "radio_buttons",
+              action_id: "urgency_value",
+              initial_option: {
+                text: {
+                  type: "plain_text",
+                  text: "Anytime is fine :thumbsup:"
+                },
+                value: "Anytime is fine"
               },
-              label: {
-                type: "plain_text",
-                text: "Subject:",
-                emoji: false
-              }
-            },
-            {
-              type: "input",
-              block_id: "body",
-              element: {
-                type: "plain_text_input",
-                action_id: "body_value",
-                multiline: true,
-                initial_value: initialBody
-              },
-              label: {
-                type: "plain_text",
-                text: "Body:",
-                emoji: false
-              }
-            },
-            {
-              type: "input",
-              block_id: "urgency",
-              element: {
-                type: "radio_buttons",
-                action_id: "urgency_value",
-                initial_option: {
+              options: [
+                {
                   text: {
                     type: "plain_text",
                     text: "Anytime is fine :thumbsup:"
                   },
                   value: "Anytime is fine"
                 },
-                options: [
-                  {
-                    text: {
-                      type: "plain_text",
-                      text: "Anytime is fine :thumbsup:"
-                    },
-                    value: "Anytime is fine"
-                  },
-                  {
-                    text: {
-                      type: "plain_text",
-                      text: "Need it soon... :pray:"
-                    },
-                    value: "Need it soon"
-                  },
-                  {
-                    text: {
-                      type: "plain_text",
-                      text: "Immediate need! :warning:"
-                    },
-                    value: "Immediate need"
-                  }
-                ]
-              },
-              label: {
-                type: "plain_text",
-                text: "Urgency:",
-                emoji: false
-              }
-            },
-            {
-              type: "context",
-              elements: [
                 {
-                  type: "mrkdwn",
-                  text: `From: ${context.user_real_name} in #${command.channel_name}`
+                  text: {
+                    type: "plain_text",
+                    text: "Need it soon... :pray:"
+                  },
+                  value: "Need it soon"
+                },
+                {
+                  text: {
+                    type: "plain_text",
+                    text: "Immediate need! :warning:"
+                  },
+                  value: "Immediate need"
                 }
               ]
+            },
+            label: {
+              type: "plain_text",
+              text: "Urgency:",
+              emoji: false
             }
-          ]
-        }
-      });
-    } catch (e) {
-      console.error(e);
-      say(
-        `:email: Could not prepare email to send to ${process.env.SOLUTIONS_EMAIL}.\n\nError occurred: ${e}`
-      );
-    }
+          },
+          {
+            type: "context",
+            elements: [
+              {
+                type: "mrkdwn",
+                text: `From: ${context.user_real_name} in #${command.channel_name}`
+              }
+            ]
+          }
+        ]
+      }
+    });
+  } catch (e) {
+    console.error(e);
+    say(
+      `:email: Could not prepare email to send to ${process.env.SOLUTIONS_EMAIL}.\n\nError occurred: ${e}`
+    );
+  }
+};
+
+app.command(
+  "/solutions",
+  getUserContext,
+  async ({ context, command, body, ack, say }) => {
+    await ack();
+    await mailSolutionsWorkflow({ context, command, body, ack, say });
   }
 );
 
@@ -287,7 +284,7 @@ app.view("solutions_email", async ({ ack, payload, context }) => {
       token: context.botToken,
       channel: channel_id,
       user: user_id,
-      text: `:email: :heavy_check_mark: I sent that email to ${process.env.SOLUTIONS_EMAIL} for you, <@${user_id}>`
+      text: `:email: :heavy_check_mark: I sent that email to ${process.env.SOLUTIONS_EMAIL} for you.`
     });
   } catch (e) {
     console.error(e);
