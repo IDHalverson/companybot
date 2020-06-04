bf7bd949dfc3237ade5c0e49fbcd45d1347979ec
Ian Halverson <ihalverson@burrislogistics.com>
2020-06-04 13:44:41 -0400

    add ability to post as burrisbot if authorized

--- services/burris-bot-interactions/routes.js
+++ services/burris-bot-interactions/routes.js
@@ -1,7 +1,29 @@
 const { app } = require("../../index");
 const BurrisBot = require("./burris-bot");
+const { get } = require("lodash");
 
 const burrisBot = new BurrisBot();
+
+app.message(
+  /burrisbot\spost\sin\s\<\#([A-Za-z0-9]+)\|[a-z\-\_]+\>\s(.+)/,
+  async ({ payload, context }) => {
+    try {
+      if (
+        payload &&
+        payload.user &&
+        process.env.BOT_POSTS_USERS.split(",").includes(payload.user)
+      ) {
+        await app.client.chat.postMessage({
+          token: context.botToken,
+          channel: get(context, "matches[1]"),
+          text: get(context, "matches[2]")
+        });
+      }
+    } catch (e) {
+      console.error(e);
+    }
+  }
+);
 
 app.message(/(.)*burris(\-|\_)*bot(.)*/i, async ({ say }) => {
   await say(burrisBot.getBurrisBotGreeting());
