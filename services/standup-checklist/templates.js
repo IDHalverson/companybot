6ee895e12007cb1672996d2a9d049903ddfebf51
Ian Halverson <ihalverson@burrislogistics.com>
2020-06-10 09:48:46 -0400

    add commands tagline to BSCP standup

--- services/standup-checklist/templates.js
+++ services/standup-checklist/templates.js
@@ -1,6 +1,8 @@
 const standupTemplate = (userIds) => `Who needs to give their update:
     
-${userIds.map((userId) => `:black_small_square: <@${userId}>`).join("\n")}`;
+${userIds.map((userId) => `:black_small_square: <@${userId}>`).join("\n")}
+
+commands: [check off|uncheck|where is] @user [is on vacation]`;
 
 module.exports = {
   standupTemplate
