5ed1093ccf24b82c1c28b4730656e2b0fd22edf9
Ian Halverson <ihalverson@burrislogistics.com>
2021-05-07 11:15:10 -0400

    temporary unavailable messages

--- services/jira-unfurl/routes.js
+++ services/jira-unfurl/routes.js
@@ -3,9 +3,33 @@ const {
   jiraUnfurlCallback,
   jiraUnfurlDetailedCallback
 } = require("./buslogic");
+const axios = require("axios");
 
 app.message(/((?<!([A-Z]{1,10})-?)[A-Z0-9]+-\d+)/, jiraUnfurlCallback);
 
-app.command("/jira", jiraUnfurlCallback);
+// TEMPORARY UNTIL FIXED
+app.message(/((?<!([A-Z]{1,10})-?)(?:SCP|V4)-\d+)/, async ({ say }) => {
+  await say("Sorry, Burris Bot's JIRA integration will be repaired soon.")
+});
 
-app.command("/jira-details", jiraUnfurlDetailedCallback);
+const unavailable = async ({ context, command, ack }) => {
+  ack();
+  let postParams = {
+    token: context.botToken,
+    icon_emoji: ":jira:",
+    username: "JIRA",
+    text: "Sorry, Burris Bot's JIRA integration will be repaired soon.",
+    response_type: "in_channel"
+  };
+  axios.post(command.response_url, postParams);
+}
+
+app.command("/jira",
+  unavailable
+  // jiraUnfurlCallback
+);
+
+app.command("/jira-details",
+  unavailable
+  // jiraUnfurlDetailedCallback
+);
