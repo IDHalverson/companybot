5b2476649d03febbed01774c2dfe8c61aa963c1a
Ian Halverson <ihalverson@burrislogistics.com>
2021-09-29 13:43:44 -0400

    rename some files

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ module.exports = { app, WorkflowStep };
 require("./services/email-solutions/routes.js");
 require("./services/listen-for-here/routes.js");
 require("./services/jira-tagger/routes.js");
-require("./services/burris-bot-interactions/routes.js");
+require("./services/company-bot-interactions/routes.js.js");
 require("./services/jira-unfurl/routes.js");
 require("./services/standup-checklist/routes.js");
 require("./services/always-tag/routes.js");
