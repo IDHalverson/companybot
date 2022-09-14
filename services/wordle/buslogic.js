aaf56e92c45e64f5d051bdc9cff51d53ab2db711
Ian Halverson <ian@ndgcommunications.com>
2022-09-14 15:41:21 -0400

    add medals and sorting to wordle replay

--- services/wordle/buslogic.js
+++ services/wordle/buslogic.js
@@ -72,12 +72,58 @@ const handleWordlePosted = async ({
         usersDoneAlready.push(result.user);
       }
     }
+    const lines = accumulatedScoreboard.match(/\n\`[^\n]+/g);
+    lines.sort().reverse();
+
+    const firstPlaceIndexes = lines
+      .map((line, index) =>
+        Number(line.match(/\`([0-9]+)\`/)[1]) ===
+        Number(lines[0].match(/\`([0-9]+)\`/)[1])
+          ? index
+          : null
+      )
+      .filter((it) => it != null);
+    const secondPlaceIndexes = lines
+      .map((line, index) =>
+        Number(line.match(/\`([0-9]+)\`/)[1]) ===
+        Number(
+          lines[firstPlaceIndexes.slice(-1)[0] + 1]?.match(/\`([0-9]+)\`/)[1]
+        )
+          ? index
+          : null
+      )
+      .filter((it) => it != null);
+    const thirdPlaceIndexes = lines
+      .map((line, index) =>
+        Number(line.match(/\`([0-9]+)\`/)[1]) ===
+        Number(
+          lines[secondPlaceIndexes.slice(-1)[0] + 1]?.match(/\`([0-9]+)\`/)[1]
+        )
+          ? index
+          : null
+      )
+      .filter((it) => it != null);
+
+    firstPlaceIndexes.forEach((idx) => {
+      lines[idx] += " :first_place_medal:";
+    });
+    secondPlaceIndexes.forEach((idx) => {
+      lines[idx] += " :second_place_medal:";
+    });
+    thirdPlaceIndexes.forEach((idx) => {
+      lines[idx] += " :third_place_medal:";
+    });
+
+    const finalScoreboard = `${
+      accumulatedScoreboard.split("\n\n")[0]
+    }\n\n${lines.join("")}`;
+
     app.client.chat.postMessage({
       token: context.botToken,
       channel: payload.channel,
       username: "Word",
       icon_emoji: ":word:",
-      text: accumulatedScoreboard,
+      text: finalScoreboard,
     });
   } else {
     const wordleNumberPosted =
